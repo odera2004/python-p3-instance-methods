945a2c418c5f5a7989cf6a5048595d347e4eab2c
odera <eugeneodera59@gmail.com>
2024-11-30 10:37:31 +0300

    add text

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking.")
+mark = Person()
+mark.talk()
+mark.walk()
